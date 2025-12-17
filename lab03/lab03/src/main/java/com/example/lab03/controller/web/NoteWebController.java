c495145d07751d8043ed6f376b45117bf8c79290
Worgzz333r <golubeckola@gmail.com>
2025-12-18 00:04:21 +0200

    lab3 ready
    Added a comment in the NoteWebController class to clarify the base path for all methods.

--- lab03/lab03/src/main/java/com/example/lab03/controller/web/NoteWebController.java
+++ lab03/lab03/src/main/java/com/example/lab03/controller/web/NoteWebController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/web/notes")
+@RequestMapping("/web/notes") // Базовий шлях для всіх методів
 public class NoteWebController {
 
     private final NoteService noteService;
