051604bdd30373aeb66b524a8f4d126632285257
Worgzz333r <golubeckola@gmail.com>
2025-12-18 00:27:48 +0200

    lab3 ready
    Added comments to explain application startup process.

--- lab03/lab03/src/main/java/com/example/lab03/Lab03Application.java
+++ lab03/lab03/src/main/java/com/example/lab03/Lab03Application.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Lab03Application {
-
+	
+	// Точка входу в додаток
 	public static void main(String[] args) {
+		// Запускає Spring Boot додаток
 		SpringApplication.run(Lab03Application.class, args);
+		// Після цього:
+		// 1. Завантажується Tomcat сервер
+		// 2. Підключається база даних
+		// 3. Реєструються всі @Bean
+		// 4. Додаток доступний на localhost:8080
 	}
-
 }
